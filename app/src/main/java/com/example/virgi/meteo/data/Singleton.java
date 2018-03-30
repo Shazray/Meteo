d278e01d873df6f1782362be3b08fa91be46e598
Shazray <virgilio.lofrano10@gmail.com>
2018-03-30 09:57:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/virgi/meteo/data/Singleton.java
@@ -0,0 +1,13 @@
+package com.example.virgi.meteo.data;
+
+/**
+ * Created by virgi on 30/03/2018.
+ */
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton(){}
+
+}
---
+++ app/src/main/java/com/example/virgi/meteo/ui/activity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.virgi.meteo.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.virgi.meteo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_change: {
+
+
+                return true;
+            }
+            default: {
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+}
