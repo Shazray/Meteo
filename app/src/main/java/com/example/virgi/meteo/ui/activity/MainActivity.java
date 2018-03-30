4f63292a0de6fb69d0277dab3a1ccec56331780a
Shazray <virgilio.lofrano10@gmail.com>
2018-03-30 11:09:29 +0200

    Creazione del Singleton e della classe City

--- app/src/main/java/com/example/virgi/meteo/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/virgi/meteo/ui/activity/MainActivity.java
@@ -2,18 +2,30 @@ package com.example.virgi.meteo.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.virgi.meteo.R;
+import com.example.virgi.meteo.ui.adapter.MyRecyclerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView myRecyclerView;
+    private RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> myAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        myAdapter = new MyRecyclerAdapter();
+        myRecyclerView.setAdapter(myAdapter);
 
 
     }
@@ -32,12 +44,13 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_change: {
 
-
-                return true;
             }
             default: {
                 return super.onOptionsItemSelected(item);
             }
         }
     }
+
+
+
 }
